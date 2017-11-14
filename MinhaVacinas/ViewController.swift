c7d3089ed0089ec938722464a549fb36f6618b25
alyson <alysongirao@gmail.com>
2017-11-14 14:34:28 -0300

    Initial Commit

---
+++ MinhaVacinas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MinhaVacinas
+//
+//  Created by Alyson Brito Girão on 14/11/17.
+//  Copyright © 2017 Alyson Brito Girão. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
