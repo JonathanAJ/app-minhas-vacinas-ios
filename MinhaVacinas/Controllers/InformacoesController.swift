295c526807e2e48ad92d77f5753fe79d5b4ee88b
alyson <alysongirao@gmail.com>
2017-11-16 14:33:43 -0300

    Terminado layout inicial de informacoes

---
+++ MinhaVacinas/Controllers/InformacoesController.swift
@@ -0,0 +1,9 @@
+//
+//  InformacoesController.swift
+//  MinhaVacinas
+//
+//  Created by Alyson Brito Girão on 16/11/17.
+//  Copyright © 2017 Alyson Brito Girão. All rights reserved.
+//
+
+import Foundation
