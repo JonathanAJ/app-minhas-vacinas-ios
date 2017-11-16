da9b654a44c90d69951721e9b22ceba082413ca1
alyson <alysongirao@gmail.com>
2017-11-16 14:58:23 -0300

    Adicionado o InfoController

--- MinhaVacinas/Controllers/InformacoesController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  InformacoesController.swift
-//  MinhaVacinas
-//
-//  Created by Alyson Brito Girão on 16/11/17.
-//  Copyright © 2017 Alyson Brito Girão. All rights reserved.
-//
-
-import Foundation
