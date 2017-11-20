0d26d6134f87bafa5f26f4a958b6b6e91bc051ce
alyson <alysongirao@gmail.com>
2017-11-20 14:44:45 -0300

    Melhorada organização do projeto

---
+++ MinhaVacinas/Controllers/NetworkDAO.swift
@@ -0,0 +1,40 @@
+//
+//  NetworkController.swift
+//  MinhaVacinas
+//
+//  Created by Alyson Brito Girão on 16/11/17.
+//  Copyright © 2017 Alyson Brito Girão. All rights reserved.
+//
+import UIKit
+
+struct NetworkController {
+    func printdata() {
+        let url = URL(string: "http://dados.recife.pe.gov.br/dataset/d1f51b00-8b90-4c26-8e27-6f559c34dbea/resource/8222148c-14d1-47ba-ae0e-39c879246284/download/calendariovacinas.csv")
+        
+        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            guard error == nil else {
+                print(error!)
+                return
+            }
+            guard let data = data else {
+                print("Data is empty")
+                return
+            }
+            
+            print(data)
+        }
+        task.resume()
+        
+    }
+    
+//    func leAsVacinas() -> String! {
+//        if let url = NSURL(string: "http://dados.recife.pe.gov.br/dataset/d1f51b00-8b90-4c26-8e27-6f559c34dbea/resource/8222148c-14d1-47ba-ae0e-39c879246284/download/calendariovacinas.csv"){
+//            do {
+//                return try String(contentsOf: url, usedEncoding: nil)
+//            } catch {
+//                print("Não foi possível ler o conteúdo do arquivo")
+//                return nil
+//            }
+//        }
+//    }
+}
