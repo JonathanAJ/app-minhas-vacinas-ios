99f7fb667aef577819d1ae785901c2197d277674
alyson <alysongirao@gmail.com>
2017-11-30 16:26:43 -0300

    Mapa agora abre na localizacao inicial do usuario

--- MinhaVacinas/Controllers/LocaisViewController.swift
+++ MinhaVacinas/Controllers/LocaisViewController.swift
@@ -8,23 +8,52 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class LocaisViewController: UIViewController {
-    
-    
+class LocaisViewController: UIViewController, CLLocationManagerDelegate {
     
     @IBOutlet weak var mapa: MKMapView!
+    
+    // Usado para começar a pegar a localização do usuário
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
+//        let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
+//        centerMapLocation(location: initialLocation)
+        
+        locationManager.requestAlwaysAuthorization()
+        
+        // Se o servico de localizacao estiver habilitado, pegar a localizacao do usuário
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyKilometer
+            locationManager.startUpdatingLocation()
+            print("Começou a procurar a localizacao")
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            centerMapOnLocation(location: location)
+            
+        }
+        
+    }
+    
+    
+    func centerMapOnLocation(location: CLLocation) {
+        let regionRadius: CLLocationDistance = 1000
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius, regionRadius)
+        mapa.setRegion(coordinateRegion, animated: true)
     }
     
     
