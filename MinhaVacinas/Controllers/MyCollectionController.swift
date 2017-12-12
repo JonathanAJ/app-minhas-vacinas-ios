a71fdc9dbc8cb93e1ad0e8f113b80c908715eb12
Jonathan Alves <alves_aj@windowslive.com>
2017-12-12 12:58:10 -0300

    muda cor label, botao centraliza, nome app, splash

--- MinhaVacinas/Controllers/MyCollectionController.swift
+++ MinhaVacinas/Controllers/MyCollectionController.swift
@@ -35,12 +35,12 @@ class MyCollectionCellController: UICollectionViewCell {
         self.myLabelAge.text = age
         
         var badgeAppearance = BadgeAppearance()
-        badgeAppearance.backgroundColor = UIColor.blue //default is red
+        badgeAppearance.backgroundColor = UIColor.gray
         badgeAppearance.textColor = UIColor.white // default is white
         badgeAppearance.textAlignment = .center //default is center
         badgeAppearance.textSize = 10 //default is 12
-        badgeAppearance.distanceFromCenterX = 20 //default is 0
-        badgeAppearance.distanceFromCenterY = 2 //default is 0
+        badgeAppearance.distanceFromCenterX = 1 //default is 0
+        badgeAppearance.distanceFromCenterY = 3 //default is 0
         self.myView.badge(text: "\(String(format:"%.0f", progress * 100))%", appearance: badgeAppearance)
     }
 }
